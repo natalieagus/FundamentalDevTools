c15e490d863ffd748b2c165f565d733009d80bef
Ubuntu <ubuntu@ip-172-31-47-124.ap-southeast-1.compute.internal>
2021-09-02 05:05:36 +0000

    feat: Add delay on server and https-server

--- https-server.js
+++ https-server.js
@@ -1,12 +1,16 @@
-const https = require('https');
-const fs = require('fs');
+const https = require("https");
+const fs = require("fs");
 
 const options = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem')
+  key: fs.readFileSync("key.pem"),
+  cert: fs.readFileSync("cert.pem"),
 };
 
-https.createServer(options, function (req, res) {
-  res.writeHead(200);
-  res.end("hello world\n");
-}).listen(8080);
+https
+  .createServer(options, function (req, res) {
+    res.writeHead(200);
+    setInterval(() => {
+      res.end("hello world\n");
+    }, 1000);
+  })
+  .listen(8080);
--- server.js
+++ server.js
@@ -2,15 +2,17 @@
 
 const http = require("http");
 
-const host = '0.0.0.0';
+const host = "0.0.0.0";
 const port = 8000;
 
 const requestListener = function (req, res) {
-    res.writeHead(200);
+  res.writeHead(200);
+  setInterval(() => {
     res.end("My first server!");
+  }, 1000);
 };
 
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
+  console.log(`Server is running on http://${host}:${port}`);
 });
